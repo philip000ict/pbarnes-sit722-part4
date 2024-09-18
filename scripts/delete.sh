2bdf66da63de88ff905ea7af8abdc36670f65aef
philip000ict <philip000ict@gmail.com>
2024-09-18 15:05:56 +1000

    teraform bash scripts

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,1 +1,16 @@
 # Your Solution
+# 
+# Remove containers from Kubernetes.
+#
+# Environment variables:
+#
+#   NAME - The name of the microservice to delete.
+#
+# Usage:
+#
+#   ./scripts/cd/delete.sh
+#
+set -u # or set -o nounset
+: "$NAME"
+
+envsubst < ./scripts/cd/${NAME}.yaml | kubectl delete -f -
