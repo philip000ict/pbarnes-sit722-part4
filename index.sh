e13b0438021aa90cfd71da9c9917499aa4ea3b47
philip000ict <philip000ict@gmail.com>
2024-09-28 09:22:17 +1000

    add index.sh

---
+++ index.sh
@@ -0,0 +1,5 @@
+# This is a shell script that simply prints Hello World.
+# When you push changes to this shell script GitHub Action automatically runs it.
+
+echo "Hello Oh Johnny O by Jimminy!"
+echo "Hello One Johnny 1 by Jommony!"
